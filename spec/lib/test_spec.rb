da2a045a797d606da639378432ab8d5458b65f97
RobertoDip <dip.jesusr@gmail.com>
2013-11-02 14:55:43 -0300

    [Test Suite] 	- Adds Rspec as a test suite 	- Adds basic configuration and pending specs

---
+++ spec/lib/test_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe MarionetteDust do
+  xit "should include Underscore files" do
+  end
+  xit "should include Backbone files" do
+  end
+  xit "should include Marionette files" do
+  end
+  xit "should include Dust files" do
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require 'marionette_dust'
