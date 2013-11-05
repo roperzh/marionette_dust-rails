8550bb678418daab1066912e5aa403fc85d55b16
Pablo Barrios <ingpablobarrios@gmail.com>
2013-11-05 10:19:22 -0300

    TESTS FTW: added basic tests support files and covered the install generator

---
+++ test/lib/generators/marionette_dust/install/templates/app/assets/javascripts/app.js
@@ -0,0 +1,20 @@
+var Class = new Marionette.Application();
+
+Class.addRegions({
+  //Add application regions here
+});
+
+//Navigate to specific route
+Class.navigate = function (route, options) {
+  options || (options = {});
+  Backbone.history.navigate(route, options);
+};
+
+//Returns current route
+Class.getCurrentRoute = function () {
+  return Backbone.history.fragment;
+};
+
+Class.on('initialize:after', function () {
+  //Things to do after app initialize
+});
