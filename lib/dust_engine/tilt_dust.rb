03a4ed105df84ffaf07bbd5900c1a61b668a5988
Donovan <donovan@donovan.mn>
2014-03-27 21:09:58 -0500

    Make template self executing when not using JST, add unit test

--- lib/dust_engine/tilt_dust.rb
+++ lib/dust_engine/tilt_dust.rb
@@ -1,23 +1,40 @@
 require 'tilt'
 
 module MarionetteDust
+
   class TiltDust < Tilt::Template
+
     def self.default_mime_type
       'application/javascript'
+    end
+
+    def prepare
+      @preamble = "(function (ctx, callback) {\n"
+      @postamble = "})"
+      @postamble << "();" unless jst?
+      @postamble << "\n"
     end
 
     def evaluate(scope, locals, &block)
       template_name = scope.logical_path.to_s.gsub('"', "")
       compiled = Dust.precompile(data, template_name)
-      <<-TEMPLATE
-        (function(ctx, callback) {
-          dust.loadSource(#{compiled.inspect});
-          dust.render('#{template_name}', ctx, callback);
-        })
-      TEMPLATE
+
+      template = "  dust.loadSource(#{compiled.inspect});\n"
+
+      if jst?
+        template << "  dust.render('#{template_name}', ctx, callback);\n"
+      end
+
+      combined(template)
     end
 
-    def prepare; end
+    def combined(template)
+      @preamble + template + @postamble
+    end
+
+    def jst?
+      File.basename(@file).split('.').include? 'jst'
+    end
 
   end
 end
